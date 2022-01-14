d16cf415a7caa8cd73a3d01094dcfa7d74b0fc02
Bruno Lessa <brunolessa@live.com>
2022-01-14 04:05:03 -0300

    choose profile picture

--- app/src/main/java/com/example/instagramclone/activity/EditPerfilActivity.java
+++ app/src/main/java/com/example/instagramclone/activity/EditPerfilActivity.java
@@ -1,9 +1,18 @@
 package com.example.instagramclone.activity;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,13 +21,21 @@ import com.example.instagramclone.databinding.ActivityEditPerfilBinding;
 import com.example.instagramclone.model.User;
 import com.example.instagramclone.util.FirebaseUtils;
 import com.example.instagramclone.util.UserFirebase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Objects;
 
 public class EditPerfilActivity extends AppCompatActivity {
 
     private ActivityEditPerfilBinding binding;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+    private User user;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +48,83 @@ public class EditPerfilActivity extends AppCompatActivity {
         // disable the email field
         binding.textEmail.setFocusable(false);
 
+        user = UserFirebase.getDataCurrentUser();
+        storageReference = FirebaseUtils.getFirebaseStorage();
+
+        activityResultLauncher = launcher();
+
         settingsToolbar();
         setDataCurrentUser();
         clickButtonSave();
+        clickButtonSelectPhoto();
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return false;
+    }
+
+    private ActivityResultLauncher<Intent> launcher() {
+        return registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+
+                    try {
+
+                        Uri url = result.getData().getData();
+                        Bitmap image = MediaStore.Images.Media.getBitmap(getContentResolver(), url);
+                        if (image != null) {
+                            binding.imagePerfilEdit.setImageBitmap(image);
+
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            image.compress(Bitmap.CompressFormat.JPEG, 75, baos);
+                            byte[] dataImage = baos.toByteArray();
+
+                            StorageReference imageRef = storageReference
+                                    .child("images")
+                                    .child("perfil")
+                                    .child(user.getId() + ".jpeg");
+
+                            UploadTask uploadTask = imageRef.putBytes(dataImage);
+                            uploadTask.addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(EditPerfilActivity.this,
+                                            "Erro ao fazer upload da image",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    Toast.makeText(EditPerfilActivity.this,
+                                            "Sucesso ao fazer upload da image",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+    }
+
+
+    private void clickButtonSelectPhoto(){
+        binding.textChangePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    activityResultLauncher.launch(intent);
+                }
+            }
+        });
     }
 
     private void clickButtonSave(){
@@ -52,7 +137,6 @@ public class EditPerfilActivity extends AppCompatActivity {
                 UserFirebase.updateUserName(newName);
 
                 // save in database
-                User user = UserFirebase.getDataCurrentUser();
                 user.setName(newName);
                 user.update();
 
